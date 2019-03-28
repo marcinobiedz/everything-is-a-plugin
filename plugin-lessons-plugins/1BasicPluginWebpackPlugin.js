55827c1fa1e67b69d8fa401896c3d3d237d854ad
Marcin <marcin.obiedz@gmail.com>
2019-03-28 22:51:19 +0100

    basic plugin class

--- plugin-lessons-plugins/1BasicPluginWebpackPlugin.js
+++ plugin-lessons-plugins/1BasicPluginWebpackPlugin.js
@@ -1,19 +1,7 @@
-const pluginUtils = require("./plugin-utils");
-const chalk = require("chalk");
 
 class BasicPlugin {
-    constructor() {
 
-        pluginUtils.logPluginEvent("pluginDidMount", "BasicPlugin");
-    }
-    apply(compiler) {
-        compiler.plugin(["run","watch-run"], (compiler, callback) => {
-            const pinkMessage = chalk.magenta("\n\nHello World\n\n");
-            console.log(pinkMessage);
-
-            callback();
-        });
-    }
+    apply(compiler) {}
 }
 
 module.exports = BasicPlugin;
