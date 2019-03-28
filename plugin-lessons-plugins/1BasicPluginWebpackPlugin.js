1f85f76418e4b8e836ac546f32a05a92979bf56d
Marcin <marcin.obiedz@gmail.com>
2019-03-28 22:49:49 +0100

    two hooks in one

--- plugin-lessons-plugins/1BasicPluginWebpackPlugin.js
+++ plugin-lessons-plugins/1BasicPluginWebpackPlugin.js
@@ -7,20 +7,12 @@ class BasicPlugin {
         pluginUtils.logPluginEvent("pluginDidMount", "BasicPlugin");
     }
     apply(compiler) {
-
-        compiler.plugin("run", (compiler, callback) => {
+        compiler.plugin(["run","watch-run"], (compiler, callback) => {
             const pinkMessage = chalk.magenta("\n\nHello World\n\n");
             console.log(pinkMessage);
 
             callback();
         });
-        compiler.plugin("watch-run", (compiler, callback) => {
-            const pinkMessage = chalk.magenta("\n\nHello World\n\n");
-            console.log(pinkMessage);
-
-            callback();
-        });
-
     }
 }
 
